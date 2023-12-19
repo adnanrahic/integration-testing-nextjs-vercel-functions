d70a9d0bf56be27e46c4a785df9f31c4c2b0929f
Adnan Rahic <ado.raha198@gmail.com>
2023-12-19 16:58:58 +0100

    try http exporter

--- instrumentation.node.ts
+++ instrumentation.node.ts
@@ -1,12 +1,11 @@
 import { NodeSDK } from '@opentelemetry/sdk-node'
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { Resource } from '@opentelemetry/resources'
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch'
 
 console.log(process.env.OTEL_EXPORTER_OTLP_ENDPOINT)
-console.log(process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT)
 
 const sdk = new NodeSDK({
   // The OTEL_EXPORTER_OTLP_ENDPOINT env var is passed into "new OTLPTraceExporter" automatically.
