29ea8c5fa51c602ceb8661dcd56aeca7ada9939e
Adnan Rahic <ado.raha198@gmail.com>
2023-12-19 15:52:59 +0100

    try grcp export

--- instrumentation.node.ts
+++ instrumentation.node.ts
@@ -1,5 +1,5 @@
 import { NodeSDK } from '@opentelemetry/sdk-node'
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
 import { Resource } from '@opentelemetry/resources'
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
@@ -9,7 +9,7 @@ const sdk = new NodeSDK({
   // The OTEL_EXPORTER_OTLP_ENDPOINT env var is passed into "new OTLPTraceExporter" automatically.
   // If the OTEL_EXPORTER_OTLP_ENDPOINT env var is not set the "new OTLPTraceExporter" will
   // default to use "http://localhost:4317" for gRPC and "http://localhost:4318" for HTTP.
-  // This sample is using HTTP.
+  // This sample is using gRPC.
   traceExporter: new OTLPTraceExporter(),
   instrumentations: [
     getNodeAutoInstrumentations(),
